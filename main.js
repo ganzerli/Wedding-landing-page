d4a1d84b5a3240c6ed013cbc9f33327709c42b6c
Massimo Ganzerli <massimoganzerli90@gmail.com>
2020-06-16 18:56:33 +0200

    parallax and form img

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded",()=>{
-//   nada
+    
+    AOS.init();
+    var rellax = new Rellax('.rellax');
+    
+    // prevent form submit
+    const fakeSubmit = e => e.preventDefault();
+    const form = document.querySelector("form");
+    form.onsubmit = fakeSubmit;
 });
